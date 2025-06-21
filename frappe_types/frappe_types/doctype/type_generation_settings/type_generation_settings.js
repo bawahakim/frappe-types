3743d47d55ba5d2983f6ccc3671e697158c12f04
Hakim Bawa <bawahakim@gmail.com>
2025-06-21 14:35:03 +0000

    lint: js files

--- frappe_types/frappe_types/doctype/type_generation_settings/type_generation_settings.js
+++ frappe_types/frappe_types/doctype/type_generation_settings/type_generation_settings.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 // frappe.ui.form.on('Type Generation Settings', {
-	// refresh: function(frm) {
+// refresh: function(frm) {
 
-	// }
+// }
 // });
