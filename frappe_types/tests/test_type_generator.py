8837825f39d52882a29704be1f230446f66272ab
Hakim Bawa <bawahakim@gmail.com>
2025-06-21 13:29:23 +0000

    refactor: utils method

--- frappe_types/tests/test_type_generator.py
+++ frappe_types/tests/test_type_generator.py
@@ -3,7 +3,7 @@ from frappe.tests.utils import FrappeTestCase
 from frappe_types.frappe_types.type_generator import TypeGenerator
 import os
 import shutil
-from frappe_types.tests.utils import TestTypeGeneratorUtils, sanitize_content, get_expected_ts_file, default_fields, updated_fields
+from frappe_types.tests.utils import TestTypeGeneratorUtils, sanitize_content
 
 class TestTypeGenerator(FrappeTestCase):
     def tearDown(self) -> None:
@@ -28,39 +28,39 @@ class TestTypeGenerator(FrappeTestCase):
         generator = TypeGenerator(app_name="frappe_types")
 
         generator.generate_doctype(TestTypeGeneratorUtils.test_doctype_name)
-        with open(TestTypeGeneratorUtils.types_output_path, "r") as f:
+        with open(TestTypeGeneratorUtils.get_types_output_path(), "r") as f:
             content = f.read()
-            self.assertEqual(sanitize_content(content), get_expected_ts_file(with_child_table=False))
+            self.assertEqual(sanitize_content(content), TestTypeGeneratorUtils.get_expected_ts_file(with_child_table=False))
 
     def test_generate_types_for_doctype_with_child_table(self):
         generator = TypeGenerator(app_name="frappe_types", generate_child_tables=True)
 
         generator.generate_doctype(TestTypeGeneratorUtils.test_doctype_name)
-        with open(TestTypeGeneratorUtils.types_output_path, "r") as f:
+        with open(TestTypeGeneratorUtils.get_types_output_path(), "r") as f:
             content = f.read()
-            self.assertEqual(sanitize_content(content), get_expected_ts_file(with_child_table=True))
+            self.assertEqual(sanitize_content(content), TestTypeGeneratorUtils.get_expected_ts_file(with_child_table=True))
     
     def test_generate_types_for_module(self):
         generator = TypeGenerator(app_name="frappe_types")
 
         generator.generate_module(TestTypeGeneratorUtils.module)
-        with open(TestTypeGeneratorUtils.types_output_path, "r") as f:
+        with open(TestTypeGeneratorUtils.get_types_output_path(), "r") as f:
             content = f.read()
-            self.assertEqual(sanitize_content(content), get_expected_ts_file(with_child_table=True))
+            self.assertEqual(sanitize_content(content), TestTypeGeneratorUtils.get_expected_ts_file(with_child_table=True))
 
     def test_updates_types(self):
         doc = frappe.get_doc("DocType", TestTypeGeneratorUtils.test_doctype_name)
         doc.append("fields", {"fieldname": "data_field_new", "fieldtype": "Data", "label": "Data Field New"})
         doc.save()
 
-        with open(TestTypeGeneratorUtils.types_output_path, "r") as f:
+        with open(TestTypeGeneratorUtils.get_types_output_path(), "r") as f:
             content = f.read()
-            self.assertEqual(sanitize_content(content), get_expected_ts_file(with_updated_fields=True))
+            self.assertEqual(sanitize_content(content), TestTypeGeneratorUtils.get_expected_ts_file(with_updated_fields=True))
 
     def test_generation_paused(self):
         frappe.conf["frappe_types_pause_generation"] = 1
         generator = TypeGenerator(app_name="frappe_types")
         generator.generate_doctype(TestTypeGeneratorUtils.test_doctype_name)
 
-        self.assertFalse(os.path.exists(TestTypeGeneratorUtils.types_output_path))
+        self.assertFalse(os.path.exists(TestTypeGeneratorUtils.get_types_output_path()))
 
--- frappe_types/tests/utils.py
+++ frappe_types/tests/utils.py
@@ -8,10 +8,6 @@ class TestTypeGeneratorUtils:
     test_doctype_name = "Test Generated DocType"
     doctype_child_name = f"{test_doctype_name} Child Table"
 
-    test_dir = os.path.dirname(__file__)
-    types_base_path = os.path.join(test_dir, 'types')
-    types_output_path = os.path.join(types_base_path, module.replace(" ", ""), 'TestGeneratedDocType.ts')
-
     @classmethod
     def generate_test_doctype(cls):
         cls._generate_test_doctype_child_table()
@@ -83,9 +79,32 @@ class TestTypeGeneratorUtils:
         doctype.append("permissions", {"role": "System Manager"})
         doctype.insert()
 
-expected_template = """\
+    @classmethod
+    def get_expected_ts_file(cls, with_child_table: bool = False, with_updated_fields: bool = False) -> str:
+        fields = [cls._default_fields]
+
+        if with_child_table:
+            fields.append(cls._render_table_field_linked())
+        else:
+            fields.append(cls._render_table_field_any())
+        
+        if with_updated_fields:
+            fields.append(cls._updated_fields)
+
+        formatted = cls._render_base_template(fields="\n".join(fields), import_child_table=cls._render_import_child_table() if with_child_table else "")
+        return sanitize_content(formatted)
+
+    @classmethod
+    def get_types_output_path(cls) -> str:
+        cls.test_dir = os.path.dirname(__file__)
+        cls.types_base_path = os.path.join(cls.test_dir, 'types')
+        return os.path.join(cls.types_base_path, to_ts_type(cls.module), 'TestGeneratedDocType.ts')
+
+    @classmethod
+    def _render_base_template(cls, fields: str, import_child_table: str) -> str:
+        return f"""\
 {import_child_table}
-export interface TestGeneratedDocType{{
+export interface {to_ts_type(cls.test_doctype_name)}{{
     name: string
     creation: string
     modified: string
@@ -100,78 +119,84 @@ export interface TestGeneratedDocType{{
 }}
 """
 
-default_fields = """\
-	/**	Data Field : Data	*/
-	data_field?: string
-	/**	Int Field : Int	*/
-	int_field?: number
-	/**	Link Field : Link - DocType	*/
-	link_field?: string
-	/**	Dynamic Link Field : Dynamic Link	*/
-	dynamic_link_field?: string
-	/**	Read Only Field : Read Only	*/
-	read_only_field?: string
-	/**	Password Field : Password	*/
-	password_field?: string
-	/**	Check Field : Check	*/
-	check_field?: 0 | 1
-	/**	Float Field : Float	*/
-	float_field?: number
-	/**	Currency Field : Currency	*/
-	currency_field?: number
-	/**	Percent Field : Percent	*/
-	percent_field?: number
-	/**	Attach Field : Attach	*/
-	attach_field?: string
-	/**	Attach Image Field : Attach Image	*/
-	attach_image_field?: string
-	/**	Image Field : Image	*/
-	image_field?: string
-	/**	Code Field : Code	*/
-	code_field?: string
-	/**	Long Text Field : Long Text	*/
-	long_text_field?: string
-	/**	Small Text Field : Small Text	*/
-	small_text_field?: string
-	/**	Text Field : Text	*/
-	text_field?: string
-	/**	Text Editor Field : Text Editor	*/
-	text_editor_field?: string
-	/**	Markdown Field : Markdown Editor	*/
-	markdown_field?: string
-	/**	Date Field : Date	*/
-	date_field?: string
-	/**	Datetime Field : Datetime	*/
-	datetime_field?: string
-	/**	Time Field : Time	*/
-	time_field?: string
-	/**	Phone Field : Phone	*/
-	phone_field?: string
-	/**	Color Field : Color	*/
-	color_field?: string
-	/**	Duration Field : Duration	*/
-	duration_field?: string
-	/**	Select Field : Select	*/
-	select_field?: "Option 1" | "Option 2"
+    _default_fields = """\
+    /**	Data Field : Data	*/
+    data_field?: string
+    /**	Int Field : Int	*/
+    int_field?: number
+    /**	Link Field : Link - DocType	*/
+    link_field?: string
+    /**	Dynamic Link Field : Dynamic Link	*/
+    dynamic_link_field?: string
+    /**	Read Only Field : Read Only	*/
+    read_only_field?: string
+    /**	Password Field : Password	*/
+    password_field?: string
+    /**	Check Field : Check	*/
+    check_field?: 0 | 1
+    /**	Float Field : Float	*/
+    float_field?: number
+    /**	Currency Field : Currency	*/
+    currency_field?: number
+    /**	Percent Field : Percent	*/
+    percent_field?: number
+    /**	Attach Field : Attach	*/
+    attach_field?: string
+    /**	Attach Image Field : Attach Image	*/
+    attach_image_field?: string
+    /**	Image Field : Image	*/
+    image_field?: string
+    /**	Code Field : Code	*/
+    code_field?: string
+    /**	Long Text Field : Long Text	*/
+    long_text_field?: string
+    /**	Small Text Field : Small Text	*/
+    small_text_field?: string
+    /**	Text Field : Text	*/
+    text_field?: string
+    /**	Text Editor Field : Text Editor	*/
+    text_editor_field?: string
+    /**	Markdown Field : Markdown Editor	*/
+    markdown_field?: string
+    /**	Date Field : Date	*/
+    date_field?: string
+    /**	Datetime Field : Datetime	*/
+    datetime_field?: string
+    /**	Time Field : Time	*/
+    time_field?: string
+    /**	Phone Field : Phone	*/
+    phone_field?: string
+    /**	Color Field : Color	*/
+    color_field?: string
+    /**	Duration Field : Duration	*/
+    duration_field?: string
+    /**	Select Field : Select	*/
+    select_field?: "Option 1" | "Option 2"
 """
 
-updated_fields = """\
+    _updated_fields = """\
     /**	Data Field New : Data	*/
     data_field_new?: string
 """
 
-table_field_linked = """\
-    /**	Table Field : Table - Test Generated DocType Child Table	*/
-    table_field?: TestGeneratedDocTypeChildTable[]
+    @classmethod
+    def _render_table_field_linked(cls) -> str:
+        return f"""\
+    /**	Table Field : Table - {cls.doctype_child_name}	*/
+    table_field?: {to_ts_type(cls.doctype_child_name)}[]
 """
 
-table_field_any = """\
-    /**	Table Field : Table - Test Generated DocType Child Table	*/
+    @classmethod
+    def _render_table_field_any(cls) -> str:
+        return f"""\
+    /**	Table Field : Table - {cls.doctype_child_name}	*/
     table_field?: any
 """
 
-import_child_table = """\
-import { TestGeneratedDocTypeChildTable } from './TestGeneratedDocTypeChildTable'
+    @classmethod
+    def _render_import_child_table(cls) -> str:
+        return f"""\
+import {{ {to_ts_type(cls.doctype_child_name)} }} from './{to_ts_type(cls.doctype_child_name)}'
 """
 
 def sanitize_content(text: str) -> str:
@@ -186,16 +211,5 @@ def sanitize_content(text: str) -> str:
     ]
     return "\n".join(cleaned_lines).rstrip()
 
-def get_expected_ts_file(with_child_table: bool = False, with_updated_fields: bool = False) -> str:
-    fields = [default_fields]
-
-    if with_child_table:
-        fields.append(table_field_linked)
-    else:
-        fields.append(table_field_any)
-    
-    if with_updated_fields:
-        fields.append(updated_fields)
-
-    formatted = expected_template.format(fields="\n".join(fields), import_child_table=import_child_table if with_child_table else "")
-    return sanitize_content(formatted)
+def to_ts_type(field_type: str) -> str:
+    return field_type.replace(" ", "")
