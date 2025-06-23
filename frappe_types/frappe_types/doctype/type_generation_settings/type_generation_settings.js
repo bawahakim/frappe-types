5d1b9dc36e8705b1ef9e684cf0520dbbc6edcf80
Hakim Bawa <bawahakim@gmail.com>
2025-06-23 13:06:09 +0000

    feat: show installed apps as dropdown

--- frappe_types/frappe_types/doctype/type_generation_settings/type_generation_settings.js
+++ frappe_types/frappe_types/doctype/type_generation_settings/type_generation_settings.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Type Generation Settings", {
-	refresh: function (frm) {
+	refresh: async function (frm) {
 		// Toggle root output path field
 		frm.toggle_enable("root_output_path", frm.doc.export_to_root);
 		// Toggle app_path field in type_settings child table
@@ -27,5 +27,15 @@ frappe.ui.form.on("Type Generation Settings", {
 				},
 			});
 		});
+
+		const { message: apps_json } = await frappe.call(
+			"frappe.core.doctype.module_def.module_def.get_installed_apps"
+		);
+		const apps = JSON.parse(apps_json);
+
+		const app_name_field = frm.fields_dict.type_settings.grid.get_docfield("app_name");
+		const rows = frm.doc.type_settings;
+		const filtered_apps = apps.filter((app) => !rows.find((row) => row.app_name === app));
+		app_name_field.options = filtered_apps;
 	},
 });
