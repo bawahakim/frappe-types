8f50ed5c692519e27e2c91d57455cbe424e8faa7
Hakim Bawa <bawahakim@gmail.com>
2025-06-24 03:20:43 +0000

    fix: properly handle mandatory state for app path, refactor js code

--- frappe_types/frappe_types/doctype/type_generation_settings/type_generation_settings.js
+++ frappe_types/frappe_types/doctype/type_generation_settings/type_generation_settings.js
@@ -2,40 +2,69 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Type Generation Settings", {
+	setup: function (frm) {
+		filter_apps_dropdown(frm);
+	},
 	refresh: async function (frm) {
 		// Toggle root output path field
 		frm.toggle_enable("root_output_path", frm.doc.export_to_root);
-		// Toggle app_path field in type_settings child table
-		if (frm.fields_dict["type_settings"]) {
-			frm.fields_dict["type_settings"].grid.toggle_enable(
-				"app_path",
-				!frm.doc.export_to_root
-			);
-		}
 
-		frm.add_custom_button("Generate All", function () {
-			frappe.call({
-				method: "frappe_types.frappe_types.type_generator.export_all_apps",
-				callback: function (r) {
-					if (r.message) {
-						frappe.show_alert({
-							title: "Success",
-							message: r.message,
-							indicator: "green",
-						});
-					}
-				},
+		update_app_path_visibility(frm);
+
+		create_generate_all_button(frm);
+	},
+	export_to_root: function (frm) {
+		update_app_path_visibility(frm);
+	},
+	validate: function (frm) {
+		update_app_path_visibility(frm);
+
+		if (!frm.doc.export_to_root) {
+			frm.doc.type_settings.forEach((row) => {
+				if (!row.app_path) {
+					frappe.throw(
+						`Row ${row.idx}: App Path is required when Export to Root is disabled`
+					);
+				}
 			});
-		});
-
-		const { message: apps_json } = await frappe.call(
-			"frappe.core.doctype.module_def.module_def.get_installed_apps"
-		);
-		const apps = JSON.parse(apps_json);
-
-		const app_name_field = frm.fields_dict.type_settings.grid.get_docfield("app_name");
-		const rows = frm.doc.type_settings;
-		const filtered_apps = apps.filter((app) => !rows.find((row) => row.app_name === app));
-		app_name_field.options = filtered_apps;
+		}
 	},
 });
+
+const update_app_path_visibility = (frm) => {
+	const type_setting_field = frm.fields_dict["type_settings"];
+
+	const { grid } = type_setting_field;
+
+	if (!grid) return;
+
+	grid.toggle_reqd("app_path", !frm.doc.export_to_root);
+	grid.toggle_enable("app_path", !frm.doc.export_to_root);
+};
+
+const filter_apps_dropdown = async (frm, cdt, cdn) => {
+	const { message: apps_json } = await frappe.call(
+		"frappe.core.doctype.module_def.module_def.get_installed_apps"
+	);
+	const apps = JSON.parse(apps_json);
+
+	const { grid } = frm.fields_dict.type_settings;
+	grid.update_docfield_property("app_name", "options", apps);
+};
+
+const create_generate_all_button = (frm) => {
+	frm.add_custom_button("Generate All", function () {
+		frappe.call({
+			method: "frappe_types.frappe_types.type_generator.export_all_apps",
+			callback: function (r) {
+				if (r.message) {
+					frappe.show_alert({
+						title: "Success",
+						message: r.message,
+						indicator: "green",
+					});
+				}
+			},
+		});
+	});
+};
