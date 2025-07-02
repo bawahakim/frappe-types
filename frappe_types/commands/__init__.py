626479ad9e772cbc0172f02a20f296c36c9380d9
Hakim Bawa <bawahakim@gmail.com>
2025-07-02 23:21:58 +0900

    feat: add import-type-gen-settings, export-types, add seed files

--- frappe_types/commands/__init__.py
+++ frappe_types/commands/__init__.py
@@ -1,11 +1,56 @@
+import json
+from pathlib import Path
+
 import click
 import frappe
 from frappe.commands import pass_context
+from frappe.utils import get_bench_path
 
 from frappe_types.frappe_types.type_generator import (
+	TypeGenerationMethod,
+	TypeGenerator,
 	generate_types_for_doctype,
 	generate_types_for_module,
 )
+
+
+@click.command("import-type-gen-settings")
+@click.option(
+	"--file",
+	help="Path to type generation settings file, defaults to frappe_types/seed/seed.json",
+	required=False,
+)
+@pass_context
+def import_type_gen_settings(context, file):
+	"""Import type generation settings from a JSON file"""
+	if not context.sites:
+		raise frappe.SiteNotSpecifiedError
+
+	if not file:
+		file = f"{get_bench_path()}/apps/frappe_types/frappe_types/seed/seed.json"
+
+	with open(file) as f:
+		settings = json.load(f)
+
+	for site in context.sites:
+		try:
+			frappe.init(site=site)
+			frappe.connect()
+			doc = frappe.get_single("Type Generation Settings")
+			for key, value in settings.items():
+				pass
+				doc.set(key, value)
+
+			doc.save(ignore_permissions=True)
+			frappe.db.commit()
+			print(
+				f"Saved type generation settings for {site} with settings:\n {json.dumps(settings, indent=4)}"
+			)
+			frappe.clear_cache(doctype="Type Generation Settings")
+		except Exception as e:
+			print(f"Error importing type generation settings for {site}: {e}")
+		finally:
+			frappe.destroy()
 
 
 @click.command("generate-types-for-doctype")
@@ -71,4 +116,31 @@ def generate_types_file_from_module(context, app, module, generate_child_tables)
 		raise frappe.SiteNotSpecifiedError
 
 
-commands = [generate_types_file_from_doctype, generate_types_file_from_module]
+@click.command(
+	"generate-types",
+	help="Generate types for all apps of the current site. Use import-type-gen-settings to configure.",
+)
+@pass_context
+def generate_types(context):
+	"""Generate types for all apps of the current site"""
+	if not context.sites:
+		raise frappe.SiteNotSpecifiedError
+
+	for site in context.sites:
+		frappe.connect(site=site)
+
+		try:
+			generator = TypeGenerator(app_name="")
+			generator.export_all_apps()
+		except Exception as e:
+			print(f"Error exporting types for {site}: {e}")
+		finally:
+			frappe.destroy()
+
+
+commands = [
+	generate_types_file_from_doctype,
+	generate_types_file_from_module,
+	generate_types,
+	import_type_gen_settings,
+]
